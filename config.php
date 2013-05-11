26453461d6122c166d7a064d1c97162d5c5f4cb8
Jagat Prajapati <jagatprajapati@lftechnoloty.comn>
2013-05-11 10:35:44 +0545

    added user tables

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
  * configuration file
  */
 /* * **************** system configuration************************** */
-define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_CONTROLLER', 'user');
 define('DEFAULT_ACTION', 'index');
 define('QUERY_STRING', true);
 
@@ -14,5 +14,5 @@ define('BASE_PATH', '');
 /* * ********* data base configuration for local***************** */
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'sumit');
+define('DB_PASSWORD', 'php');
+define('DB_NAME', 'codejammers');
