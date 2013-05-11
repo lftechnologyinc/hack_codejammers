c6f79dda68126719c1b3dbe71852af307eb43e81
mukesh <mukeshsainju@lftechnology.com>
2013-05-11 13:06:10 +0545

    create user

--- models/createuserModel.php
+++ models/createuserModel.php
@@ -10,12 +10,25 @@
 
 	  public function addUser($post)
 	  {
+
+
 		  unset($post['submit']);
 		  unset($post['rePassword']);
 
-		  $result = $this->save('users', $post);
-		  r($result);die;
-		  return $result;
+
+
+		  $result = " INSERT INTO users (fullname, email, address, phone_no,username, password, employee_id, created_date, login_time, user_group_id)
+		  VALUES ( '" . $post['fullname'] . "', '" . $post['email'] . "', '" . $post['address'] . "', '" . $post['phone_no'] . "', '" . $post['username'] . "', '" . md5($post['password']) . "', '" . $post['employee_id'] . "', '', '', '2')";
+
+
+		  echo $result;
+		  exit;
+
+
+
+
+
+		  return mysql_query($result);
 	  }
 
   }
