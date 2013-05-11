c2190824c60f90914c308111d340c61fffbd9432
Anjali <anjalishakya@lftechnology.com>
2013-05-11 15:56:56 +0545

    css design

--- views/members.php
+++ views/members.php
@@ -1,12 +1,12 @@
 <?php ?>
-<form name="checkinout" id="checkinout" action="" method="POST" />
+<div class="blocks margin-all"><form name="checkinout" id="checkinout" action="" method="POST" />
 <?php if ($this->checkIn == 1) { ?>
-                            You are currently Checked In from <?php echo $this->CheckedInDate;?>
-	  		<input type="submit" name="checkOut" value="Check Out"/>
+                            <h4>You are currently Checked In from</h4> <?php echo $this->CheckedInDate;?>
+	  		<input class="btn-large btn-red border-all" style="margin:0 45%;" type="submit" name="checkOut" value="Check Out"/>
   <?php } else { ?>
-	  		 <input class="btn-green" type="submit" name="checkIn" value="Check In"/>
+	  		 <input class="btn-green btn-large border-all" style="margin:0 45%;" type="submit" name="checkIn" value="Check In"/>
   <?php } ?>
-</form>
+</form></div>
 
 
 
