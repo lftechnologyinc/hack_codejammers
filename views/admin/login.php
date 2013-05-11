fddcee91d4d654a22d58cf17d9b14f63b702dd55
Anjali <anjalishakya@lftechnology.com>
2013-05-11 10:49:21 +0545

    css changes in login page

--- views/admin/login.php
+++ views/admin/login.php
@@ -2,18 +2,26 @@
 	<header><h3>Login</h3></header>
 	<div class="module_content">
 		<form action="index.php?controller=user&action=doLogin" method="post">
+        
+        
+
+        
+        
 			<table align="center" width="400" cellpadding="5" cellspacing="5">
-				<tr>
-					<td>Username</td>
-					<td><input type="text" name="username"/></td>
-				</tr
-				<tr>
-					<td>Password</td>
-					<td><input type="password" name="password"/></td>
+			<tr>	
+					<td><label>Username</label></td>
+					<td><div class="control-group"><input type="text" name="username"/></div></td>
 				</tr>
-				<tr>
+               <tr>
+					<td><label>Password</label></td>
+					<td><div class="control-group"><input type="password" name="password"/></div></td>
+                  
+				</tr>
+			<tr>
+            	
 					<td></td>
-					<td><input type="submit" name="submit" value="login"/></td>
+					<td><input class="btn-green" type="submit" name="submit" value="Login"/></td>
+              
 				</tr>
 			</table>
 		</form>
